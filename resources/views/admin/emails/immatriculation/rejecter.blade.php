8892dae3bdadc2c7d5e62046ef25d5ed97b55bcd
gabi-b612 <20km095@esisalama.org>
2024-09-21 20:35:25 +0200

    fixed admin views

---
+++ resources/views/admin/emails/immatriculation/rejecter.blade.php
@@ -0,0 +1,9 @@
+<p>Bonjour {{ $denomination }},</p>
+
+<p>Nous regrettons de vous informer que votre demande d'affiliation a été rejetée.</p>
+<p>Raison : <strong>{{ $raison }}</strong></p>
+
+<p>Pour toute question, veuillez nous contacter.</p>
+
+<p>Cordialement,</p>
+<p>L'équipe CNSS</p>
